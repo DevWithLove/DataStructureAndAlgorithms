a271bd3aede25b222db319dc3f70dd9aafe1fbed
Tony Mu <tony.mu@vacasa.com>
2023-02-16 18:12:04 +1000

    first commit

---
+++ DataStructureAndAlgorithms/DataStructureAndAlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructureAndAlgorithmsApp.swift
+//  DataStructureAndAlgorithms
+//
+//  Created by Tony Mu on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DataStructureAndAlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
